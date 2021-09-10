f123352dc002e5c471acf3710f288e060c0deba3
mvillanueva24 <mariorvillanueva24@gmail.com>
2021-09-10 01:33:03 -0500

    Modelo Producto

--- TiendaVirtual/store/models.py
+++ TiendaVirtual/store/models.py
@@ -9,3 +9,15 @@ class user(models.Model):
 
     class Meta:
         db_table = "store_user"
+
+class product(models.Model):
+    nombre = models.CharField(max_length=100, blank=True)
+    precio = models.FloatField(blank=True)
+    talla = models.CharField(max_length=3)		
+    categoria = models.CharField(max_length=8, blank=True)
+    descripción = models.TextField(max_length=500)
+    stock = models.PositiveIntegerField(default=1)
+    imagen = models.ImageField(upload_to='products/')
+
+    class Meta:
+        db_table = "store_product"
